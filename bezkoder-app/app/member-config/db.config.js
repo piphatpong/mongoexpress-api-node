4e5405890d419a3600925cf35b3cb70ec795feda
piphatpong <mr.phatsorn@gmail.com>
2023-05-15 08:36:02 +0000

    mongodb set name

--- bezkoder-app/app/member-config/db.config.js
+++ bezkoder-app/app/member-config/db.config.js
@@ -6,7 +6,11 @@ const {
     DB_NAME,
   } = process.env;
   
+  // module.exports = {
+  //  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+  // };
   module.exports = {
-    url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+    url: 'mongodb+srv://mrphatsorn:Samurai2517@cluster0.tifvjce.mongodb.net/googkikdb?retryWrites=true&w=majority'
   };
+
   
